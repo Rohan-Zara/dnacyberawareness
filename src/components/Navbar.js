c5a9e8477781098052d6bfd8e884421c0d06b7d5
zodnick <nirbhay.xic.samdhss@gmail.com>
2025-08-20 16:41:59 +0530

    made the navbar responsive by making it a menubar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
-
-// Assuming you have your logo image in the src/assets folder
-// Adjust the path to where your logo is located
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Navbar() {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const pages = ["Home", "Attacks", "Tips", "News", "Threat Map"];
+
   return (
     <AppBar position="sticky" color="primary" elevation={4}>
       <Toolbar>
+        {/* Logo and title section */}
         <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
           <img
             src={"imgs/logo.jpg"} // Replace with your logo path
@@ -19,12 +33,60 @@ function Navbar() {
             style={{ height: '50px', marginRight: '5px' }}
           />
         </Box>
-        <Box>
-          <Button color="inherit" component="a" href="#hero">Home</Button>
-          <Button color="inherit" component="a" href="#attacks">Attacks</Button>
-          <Button color="inherit" component="a" href="#tips">Tips</Button>
-          <Button color="inherit" component="a" href="#news">News</Button>
-          <Button color="inherit" component="a" href="#threatmap">Threat Map</Button>
+
+        {/* Mobile menu (visible on extra-small screens) */}
+        <Box sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: "block", md: "none" },
+            }}
+          >
+            {pages.map((page) => (
+              <MenuItem key={page} onClick={handleCloseNavMenu}>
+                <Button color="inherit" component="a" href={`#${page.toLowerCase().replace(' ', '')}`}>
+                  <Typography textAlign="center">{page}</Typography>
+                </Button>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+
+        {/* Desktop navigation links (visible on medium and up screens) */}
+        <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          {pages.map((page) => (
+            <Button
+              key={page}
+              color="inherit"
+              component="a"
+              href={`#${page.toLowerCase().replace(' ', '')}`}
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+              {page}
+            </Button>
+          ))}
         </Box>
       </Toolbar>
     </AppBar>
